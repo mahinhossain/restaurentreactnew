c2f2068103d58925331b5fe4072cc67c75acf92e
Md. Mahin Hossain <intern67.it@akij.net>
2021-11-15 12:16:47 +0600

    jjj

--- src/compontents/About.js
+++ src/compontents/About.js
@@ -1,5 +1,28 @@
 import React from "react";
 
 export default function About() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="row">
+        <div className="card">
+          <div className="card-header">About</div>
+          <div className="card-text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+              consequuntur iusto totam, mollitia unde ad minima iste enim error,
+              distinctio ipsum provident ab aperiam, dolorem a debitis. Iste
+              delectus sit quam accusantium architecto est, animi, tempore a
+              laudantium quos ab veniam porro nihil ipsum doloremque eveniet,
+              aperiam quaerat! Incidunt alias dolores perferendis, reiciendis ex
+              sit tenetur dolore, ea quisquam dolorem magnam fugit sint
+              distinctio consequuntur quibusdam aperiam, ab in explicabo porro!
+              Rerum officiis harum asperiores, aut cum libero facilis recusandae
+              tenetur quis alias pariatur ipsam dolores. Repellendus architecto
+              odio aliquid?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
